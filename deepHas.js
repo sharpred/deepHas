c33b447cd35e781cc5d03ac8008f99761842a212
Paul Ryan <paul.ryan@stepupsoftware.co.uk>
2015-12-18 14:26:36 +0000

    refactoring

--- deepHas.js
+++ deepHas.js
@@ -1,85 +1,106 @@
-var index,
+var indexFalse,
+    indexTrue,
+    indexer,
     reduce,
     add,
     has,
     get,
     set;
 
-function index(obj, i) {
-    "use strict";
-    try {
-        if (obj && obj.hasOwnProperty(i)) {
-            return obj[i];
+function indexer(set) {
+    return function(obj, i) {
+        "use strict";
+        try {
+            if (obj && obj.hasOwnProperty(i)) {
+                return obj[i];
+            } else if (set) {
+                obj[i] = {};
+                return obj[i];
+            }
+            return;
+        } catch(ex) {
+            console.error(ex);
+            return;
         }
-        return;
-    } catch(ex) {
-        console.error(ex);
-        return;
-    }
-}
+    };
+};
 
-function indexSet(obj, i) {
-    "use strict";
-    try {
-        if (obj && obj.hasOwnProperty(i)) {
-            return obj[i];
-        } else {
-            obj[i] = {};
-            return obj[i];
-        }
-        return;
-    } catch(ex) {
-        console.error(ex);
-        return;
-    }
-}
+indexTrue = indexer(true);
+indexFalse = indexer(false);
 
 function reduce(obj, str) {
     "use strict";
-    if ( typeof str !== "string") {
+    try {
+        if ( typeof str !== "string") {
+            return;
+        }
+        if ( typeof obj !== "object") {
+            return;
+        }
+        return str.split('.').reduce(indexFalse, obj);
+
+    } catch(ex) {
+        console.error(ex);
         return;
     }
-    if ( typeof obj !== "object") {
-        return;
-    }
-    return str.split('.').reduce(index, obj);
+
 }
 
 function add(obj, str, val) {
     "use strict";
-    if ( typeof str !== "string") {
+    try {
+        if ( typeof str !== "string") {
+            return;
+        }
+        if ( typeof obj !== "object") {
+            return;
+        }
+        if (!val) {
+            return;
+        }
+        var items = str.split('.');
+        var initial = items.slice(0, items.length - 1);
+        var last = items.slice(items.length - 1);
+        var test = initial.reduce(indexTrue, obj);
+        test[last] = val;
+    } catch(ex) {
+        console.error(ex);
         return;
     }
-    if ( typeof obj !== "object") {
-        return;
-    }
-    if (!val) {
-        return;
-    }
-    var items = str.split('.');
-    var initial = items.slice(0, items.length - 1);
-    var last = items.slice(items.length - 1);
-    var test = initial.reduce(indexSet, obj);
-    test[last] = val;
 }
 
 function has(target, path) {
     "use strict";
-    var test = reduce(target, path);
-    if ( typeof test !== "undefined") {
-        return true;
+    try {
+        var test = reduce(target, path);
+        if ( typeof test !== "undefined") {
+            return true;
+        }
+        return false;
+    } catch(ex) {
+        console.error(ex);
+        return;
     }
-    return false;
 }
 
 function get(target, path) {
     "use strict";
-    return reduce(target, path);
+    try {
+        return reduce(target, path);
+    } catch(ex) {
+        console.error(ex);
+        return;
+    }
 }
 
 function set(target, path, val) {
     "use strict";
-    return add(target, path, val);
+    try {
+        return add(target, path, val);
+    } catch(ex) {
+        console.error(ex);
+        return;
+    }
 }
 
 exports.has = has;
